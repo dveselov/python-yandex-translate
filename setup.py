5807f6d2ec5aa177b80d5f9662b869fa898f8112
John Doe <github@personalplayground.org>
2012-12-13 20:25:34 +0400

    * setup.py * Added YandexTranslateException * Fix ValueError with empty/non-json response from API

---
+++ setup.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from distutils.core import setup
+import yandex_translate
+
+setup(name='yandex_translate',
+      version=yandex_translate.__version__,
+      description='Python library for Yandex Translate API.',
+      packages=['yandex_translate'],
+      package_dir={'yandex_translate': 'yandex_translate'},
+      provides=['yandex_translate'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+      ],
+      long_description=open('README.md').read(),
+      platforms=['All'],
+)
