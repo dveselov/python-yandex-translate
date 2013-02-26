74984e0ba9951c603b972466dca2c6d60f9c0d41
Dmitrij Veselov <pyth0n3r@ya.ru>
2013-02-26 23:18:10 +0400

    Removed tests.py

--- yandex_translate/tests.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#-*-coding:utf-8-*-
-from yandex_translate import *
-
-
-translate = YandexTranslate()
-print('Current languages:', translate.langs)
-print('Detect language:', translate.detect('Привет, мир!'))
-print('Translate:', translate.translate('Привет, мир!', 'ru-en'))  # or just 'en'
