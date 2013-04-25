370adabe9244ff67effcb40ee95be6ea28fab371
Sofia Velmer <pyth0n3r@yandex.ru>
2013-04-26 00:23:39 +0400

    Some perfomance

--- yandex_translate/__init__.py
+++ yandex_translate/__init__.py
@@ -25,6 +25,18 @@ class YandexTranslate(object):
     Class for detect language of text and translate it via Yandex.Translate API
     >>> translate = YandexTranslate()
     """
+
+    error_codes = {
+        401: "ERR_KEY_INVALID",
+        402: "ERR_KEY_BLOCKED",
+        403: "ERR_DAILY_REQ_LIMIT_EXCEEDED",
+        404: "ERR_DAILY_CHAR_LIMIT_EXCEEDED",
+        413: "ERR_TEXT_TOO_LONG",
+        422: "ERR_UNPROCESSABLE_TEXT",
+        501: "ERR_LANG_NOT_SUPPORTED",
+        503: "ERR_SERVICE_NOT_AVAIBLE",
+    }
+
     def __init__(self, key=None):
         """
         Class constructor
@@ -44,16 +56,6 @@ class YandexTranslate(object):
             'detect': 'http://translate.yandex.net/api/v1/tr.json/detect?%s',
             'translate': 'http://translate.yandex.net/api/v1/tr.json/'
             'translate?%s',
-        }
-        self.error_codes = {
-            401: "ERR_KEY_INVALID",
-            402: "ERR_KEY_BLOCKED",
-            403: "ERR_DAILY_REQ_LIMIT_EXCEEDED",
-            404: "ERR_DAILY_CHAR_LIMIT_EXCEEDED",
-            413: "ERR_TEXT_TOO_LONG",
-            422: "ERR_UNPROCESSABLE_TEXT",
-            501: "ERR_LANG_NOT_SUPPORTED",
-            503: "ERR_SERVICE_NOT_AVAIBLE",
         }
         if not key:
             raise YandexTranslateException('Please, provide key for Yandex.Translate API: https://translate.yandex.ru/apikeys')
