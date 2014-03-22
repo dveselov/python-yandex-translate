e0c77f6984be31e11f5958b151b5bd6a1d62bb70
dieselpoweredkitten <me@require.pm>
2014-03-23 00:46:36 +0400

    Split translate directions and supported languages

--- yandex_translate/__init__.py
+++ yandex_translate/__init__.py
@@ -5,160 +5,151 @@ from requests.exceptions import ConnectionError
 
 
 class YandexTranslateException(Exception):
-    """
-    Default YandexTranslate exception
-    >>> YandexTranslateException("DoctestError")
-    YandexTranslateException('DoctestError',)
-    """
-    error_codes = {
-        401: "ERR_KEY_INVALID",
-        402: "ERR_KEY_BLOCKED",
-        403: "ERR_DAILY_REQ_LIMIT_EXCEEDED",
-        404: "ERR_DAILY_CHAR_LIMIT_EXCEEDED",
-        413: "ERR_TEXT_TOO_LONG",
-        422: "ERR_UNPROCESSABLE_TEXT",
-        501: "ERR_LANG_NOT_SUPPORTED",
-        503: "ERR_SERVICE_NOT_AVAIBLE",
-    }
+  """
+  Default YandexTranslate exception
+  """
+  error_codes = {
+    401: "ERR_KEY_INVALID",
+    402: "ERR_KEY_BLOCKED",
+    403: "ERR_DAILY_REQ_LIMIT_EXCEEDED",
+    404: "ERR_DAILY_CHAR_LIMIT_EXCEEDED",
+    413: "ERR_TEXT_TOO_LONG",
+    422: "ERR_UNPROCESSABLE_TEXT",
+    501: "ERR_LANG_NOT_SUPPORTED",
+    503: "ERR_SERVICE_NOT_AVAIBLE",
+  }
 
-    def __init__(self, status_code, *args, **kwargs):
-        message = self.error_codes.get(status_code)
-        super(YandexTranslateException, self).__init__(message, *args, **kwargs)
+  def __init__(self, status_code, *args, **kwargs):
+    message = self.error_codes.get(status_code)
+    super(YandexTranslateException, self).__init__(message, *args, **kwargs)
 
 
 class YandexTranslate(object):
+
+  api_url = "https://translate.yandex.net/api/{version}/tr.json/{endpoint}"
+  api_version = "v1.5"
+  api_endpoints = {
+    "langs": "getLangs",
+    "detect": "detect",
+    "translate": "translate",
+  }
+
+  def __init__(self, key=None):
     """
-    Class for detect language of text and translate it via Yandex.Translate API
-    >>> translate = YandexTranslate()
+    >>> translate = YandexTranslate("trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
+    >>> len(translate.api_endpoints)
+    3
+    >>> len(translate.error_codes)
+    8
     """
-    api_url = 'https://translate.yandex.net/api/{version}/tr.json/{endpoint}'
-    api_version = 'v1.5'
-    api_endpoints = {
-        'langs': 'getLangs',
-        'detect': 'detect',
-        'translate': 'translate',
+    if not key:
+      raise YandexTranslateException(401)
+    self.api_key = key
+
+  def url(self, endpoint):
+    """
+    Returns full URL for specified API endpoint
+    >>> translate = YandexTranslate("trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
+    >>> translate.url("langs")
+    "https://translate.yandex.net/api/v1.5/tr.json/getLangs"
+    >>> translate.url("detect")
+    "https://translate.yandex.net/api/v1.5/tr.json/detect"
+    >>> translate.url("translate")
+    "https://translate.yandex.net/api/v1.5/tr.json/translate"
+    """
+    return self.api_url.format(version=self.api_version,
+                   endpoint=self.api_endpoints[endpoint])
+
+  @property
+  def directions(self):
+    """
+    Returns list with translate directions
+    >>> translate = YandexTranslate("trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
+    >>> directions = translate.directions
+    >>> len(directions) > 0
+    True
+    """
+    try:
+      response = requests.get(self.url("langs"), params={"key": self.api_key})
+      response = response.json()
+    except ConnectionError:
+      raise YandexTranslateException(self.error_codes[503])
+    status_code = response.get("code", 200)
+    if not status_code is 200:
+      raise YandexTranslateException(status_code)
+    return response.get("dirs")
+
+  @property
+  def langs(self):
+    """
+    Returns list with supported languages
+    >>> translate = YandexTranslate("trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
+    >>> languages = translate.langs
+    >>> len(languages) > 0
+    True
+    """
+    return set(x.split("-")[0] for x in self.directions)
+
+  def detect(self, text, format="plain"):
+    """
+    Specifies language of text
+    >>> translate = YandexTranslate("trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
+    >>> result = translate.detect(text="Hello world!")
+    >>> result == "en"
+    True
+    >>> translate.detect("なのです")
+    Traceback (most recent call last):
+    YandexTranslateException: ERR_LANG_NOT_SUPPORTED
+    """
+    data = {
+      "text": text,
+      "format": format,
+      "key": self.api_key,
     }
+    try:
+      response = requests.post(self.url("detect"), data=data)
+      response = response.json()
+    except ConnectionError:
+      raise YandexTranslateException(self.error_codes[503])
+    except ValueError:
+      raise YandexTranslateException(response)
+    language = response.get("lang", None)
+    status_code = response.get("code", 200)
+    if not status_code is 200:
+      raise YandexTranslateException(status_code)
+    elif not language:
+      raise YandexTranslateException(501)
+    return language
 
-    def __init__(self, key=None):
-        """
-        Class constructor
-        >>> translate = YandexTranslate('trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e')
-        >>> len(translate.api_endpoints)
-        3
-        >>> len(translate.error_codes)
-        8
-        """
-        if not key:
-            raise YandexTranslateException(401)
-        self.api_key = key
-
-    def url(self, endpoint):
-        """
-        Returns full URL for specified API endpoint
-        :param endpoint: String with endpoint name
-        >>> translate = YandexTranslate('api-key')
-        >>> translate.url('langs')
-        'https://translate.yandex.net/api/v1.5/tr.json/getLangs'
-        >>> translate.url('detect')
-        'https://translate.yandex.net/api/v1.5/tr.json/detect'
-        >>> translate.url('translate')
-        'https://translate.yandex.net/api/v1.5/tr.json/translate'
-        """
-        return self.api_url.format(version=self.api_version,
-                                   endpoint=self.api_endpoints[endpoint])
-
-    @property
-    def langs(self, cache=True):
-        """
-        Returns a array of languages for translate
-        :returns: List with translate derections
-        >>> translate = YandexTranslate('trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e')
-        >>> languages = translate.langs
-        >>> len(languages) > 0
-        True
-        """
-        try:
-            response = requests.get(self.url('langs'),
-                                    params={'key': self.api_key})
-            response = response.json()
-        except ConnectionError:
-            raise YandexTranslateException(self.error_codes[503])
-        status_code = response.get('code', 200)
-        if not status_code is 200:
-            raise YandexTranslateException(status_code)
-        return response.get('dirs')
-
-    @property
-    def _langs(self):
-      langs = self.langs
-      return set(x.split('-')[0] for x in langs)
-
-    def detect(self, text, format='plain'):
-        """
-        Specifies the language of the text
-        :param text: A string for language detection
-        :param format: String with text format. 'plain' or 'html'.
-        :returns: String with language code in ISO format. 'en', for example.
-        >>> translate = YandexTranslate('trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e')
-        >>> result = translate.detect(text='Hello, world!')
-        >>> result == 'en'
-        True
-        >>> translate.detect('なのです')
-        Traceback (most recent call last):
-        YandexTranslateException: ERR_LANG_NOT_SUPPORTED
-        """
-        data = {
-            'text': text,
-            'format': format,
-            'key': self.api_key,
-        }
-        try:
-            response = requests.post(self.url('detect'), data=data)
-            response = response.json()
-        except ConnectionError:
-            raise YandexTranslateException(self.error_codes[503])
-        except ValueError:
-            raise YandexTranslateException(response)
-        language = response.get('lang', None)
-        status_code = response.get('code', 200)
-        if not status_code is 200:
-            raise YandexTranslateException(status_code)
-        elif not language:
-            raise YandexTranslateException(501)
-        return language
-
-    def translate(self, text, lang, format='plain'):
-        """
-        Translate text to passed language
-        :param text: Source text
-        :param lang: Result language. 'en-ru' for English to Russian translation or just 'ru' for autodetect source language and translate it to Russian.
-        :param format: 'plain' or 'html', with chars escaping or not.
-        >>> translate = YandexTranslate('trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e')
-        >>> result = translate.translate(lang='ru', text='Hello, world!')
-        >>> result['code'] == 200
-        True
-        >>> result['lang'] == 'en-ru'
-        True
-        >>> result = translate.translate('なのです', 'en')
-        Traceback (most recent call last):
-        YandexTranslateException: ERR_LANG_NOT_SUPPORTED
-        """
-        data = {
-            'text': text,
-            'format': format,
-            'lang': lang,
-            'key': self.api_key
-        }
-        try:
-            response = requests.post(self.url('translate'), data=data)
-            response = response.json()
-        except ConnectionError:
-            raise YandexTranslateException(503)
-        status_code = response.get('code', 200)
-        if not status_code is 200:
-            raise YandexTranslateException(status_code)
-        return response
+  def translate(self, text, lang, format="plain"):
+    """
+    Translates text to passed language
+    >>> translate = YandexTranslate("trnsl.1.1.20130421T140201Z.323e508a33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
+    >>> result = translate.translate(lang="ru", text="Hello, world!")
+    >>> result["code"] == 200
+    True
+    >>> result["lang"] == "en-ru"
+    True
+    >>> result = translate.translate("なのです", "en")
+    Traceback (most recent call last):
+    YandexTranslateException: ERR_LANG_NOT_SUPPORTED
+    """
+    data = {
+      "text": text,
+      "format": format,
+      "lang": lang,
+      "key": self.api_key
+    }
+    try:
+      response = requests.post(self.url("translate"), data=data)
+      response = response.json()
+    except ConnectionError:
+      raise YandexTranslateException(503)
+    status_code = response.get("code", 200)
+    if not status_code is 200:
+      raise YandexTranslateException(status_code)
+    return response
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+  import doctest
+  doctest.testmod()
--- yandex_translate/tests.py
+++ yandex_translate/tests.py
@@ -1,83 +1,80 @@
 #!/usr/bin/python
 # coding:utf-8
 import unittest
-from yandex_translate import *
+from yandex_translate import YandexTranslate, YandexTranslateException
 
 
 class YandexTranslateTest(unittest.TestCase):
 
-    def setUp(self):
-        self.translate = YandexTranslate('trnsl.1.1.20130421T140201Z.323e508a'
-                                         '33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e')
+  def setUp(self):
+    self.translate = YandexTranslate("trnsl.1.1.20130421T140201Z.323e508a"
+                                     "33e9d84b.f1e0d9ca9bcd0a00b0ef71d82e6cf4158183d09e")
 
-    def test_langs(self):
-        languages = self.translate.langs
-        self.assertGreater(len(languages), 1)
+  def test_directions(self):
+    directions = self.translate.directions
+    self.assertGreater(len(directions), 1)
 
-    def test_underscore_langs(self):
-        languages = self.translate._langs
-        self.assertEqual(languages, set(
-            [
-                u'el', u'en', u'ca', u'it',
-                u'hy', u'cs', u'et', u'az',
-                u'es', u'ru', u'nl', u'pt',
-                u'no', u'tr', u'lv', u'lt',
-                u'ro', u'pl', u'be', u'fr',
-                u'bg', u'hr', u'de', u'da',
-                u'fi', u'hu', u'sr', u'sq',
-                u'sv', u'mk', u'sk', u'uk',
-                u'sl'
-            ]
-        ))
+  def test_langs(self):
+    languages = self.translate.langs
+    self.assertEqual(languages, set(
+      [
+        u"el", u"en", u"ca", u"it",
+        u"hy", u"cs", u"et", u"az",
+        u"es", u"ru", u"nl", u"pt",
+        u"no", u"tr", u"lv", u"lt",
+        u"ro", u"pl", u"be", u"fr",
+        u"bg", u"hr", u"de", u"da",
+        u"fi", u"hu", u"sr", u"sq",
+        u"sv", u"mk", u"sk", u"uk",
+        u"sl"
+      ]
+    ))
 
-    def test_blocked_key(self):
-        translate = YandexTranslate('trnsl.1.1.20130723T112255Z.cfcd2b1ebae9f'
-                                    'ff1.86f3d1de3621e69b8c432fcdd6803bb87ef0e963')
-        with self.assertRaises(YandexTranslateException):
-            languages = translate.detect('Hello!')
+  def test_blocked_key(self):
+    translate = YandexTranslate("trnsl.1.1.20130723T112255Z.cfcd2b1ebae9f"
+                                "ff1.86f3d1de3621e69b8c432fcdd6803bb87ef0e963")
+    with self.assertRaises(YandexTranslateException, msg="ERR_KEY_BLOCKED"):
+      languages = translate.detect("Hello!")
 
-    def test_language_detection(self):
-        language = self.translate.detect(text='Hello, world!')
-        self.assertEqual(language, 'en')
+  def test_detect_language(self):
+    language = self.translate.detect(text="Hello, world!")
+    self.assertEqual(language, "en")
 
-    def test_translate(self):
-        result = self.translate.translate(u'Hello!', 'ru')
-        self.assertEqual(result['text'][0], u'Здравствуйте!')
-        self.assertEqual(result['code'], 200)
+  def test_translate(self):
+    result = self.translate.translate(u"Hello!", "ru")
+    self.assertEqual(result["text"][0], u"Здравствуйте!")
+    self.assertEqual(result["code"], 200)
 
-    def test_translate_in_another_direction(self):
-        result = self.translate.translate(u'Здравствуйте', 'en')
-        self.assertEqual(result['text'][0], u'Hello')
-        self.assertEqual(result['code'], 200)
+  def test_translate_in_another_direction(self):
+    result = self.translate.translate(u"Здравствуйте", "en")
+    self.assertEqual(result["text"][0], u"Hello")
+    self.assertEqual(result["code"], 200)
 
-    def test_language_detection_error(self):
-        with self.assertRaises(YandexTranslateException,
-                               msg="ERR_LANG_NOT_SUPPORTED"):
-            self.translate.detect('なのです')
+  def test_language_detection_error(self):
+    with self.assertRaises(YandexTranslateException, msg="ERR_LANG_NOT_SUPPORTED"):
+      self.translate.detect("なのです")
 
-    # Yandex.Translate tries to translate this as english-to-russian
-    # def test_translate_error(self):
-    #     with self.assertRaises(YandexTranslateException,
-    #                            msg="ERR_LANG_NOT_SUPPORTED"):
-    #         self.translate.translate('なのです', 'ru')
+  # Yandex.Translate tries to translate this as english-to-russian
+  # def test_translate_error(self):
+  #   with self.assertRaises(YandexTranslateException,
+  #              msg="ERR_LANG_NOT_SUPPORTED"):
+  #     self.translate.translate("なのです", "ru")
 
-    def test_without_key(self):
-        with self.assertRaises(YandexTranslateException,
-                               msg="Please, provide key for ' \
-                               'Yandex.Translate API: ' \
-                               'https://translate.yandex.ru/apikeys"):
-            translate = YandexTranslate()
+  def test_without_key(self):
+    with self.assertRaises(YandexTranslateException,
+                           msg="Please, provide key for " \
+                               "Yandex.Translate API: " \
+                               "https://translate.yandex.ru/apikeys"):
+      translate = YandexTranslate()
 
-    def test_error_long_text(self):
-        with self.assertRaises(YandexTranslateException, 
-                               msg="ERR_TEXT_TOO_LONG"):
-            self.translate.translate('hi! '*4098, 'ru')
+  def test_error_long_text(self):
+    with self.assertRaises(YandexTranslateException, msg="ERR_TEXT_TOO_LONG"):
+      self.translate.translate("hi! "*4098, "ru")
 
-    def test_invalid_key(self):
-        with self.assertRaises(YandexTranslateException,
-                               msg="ERR_KEY_INVALID"):
-            translate = YandexTranslate('my-invalid-key')
-            language = translate.detect('Hello!')
+  def test_invalid_key(self):
+    with self.assertRaises(YandexTranslateException, msg="ERR_KEY_INVALID"):
+      translate = YandexTranslate("my-invalid-key")
+      language = translate.detect("Hello!")
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+  unittest.main()
