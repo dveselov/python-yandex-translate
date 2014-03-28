f9497f37b681344c452259b228711e13826c644b
dieselpoweredkitten <me@require.pm>
2014-03-29 03:24:16 +0400

    Update yandex_translate

--- yandex_translate/__init__.py
+++ yandex_translate/__init__.py
@@ -1,4 +1,4 @@
-# coding:utf-8
+# coding: utf-8
 
 import requests
 from requests.exceptions import ConnectionError
--- yandex_translate/tests.py
+++ yandex_translate/tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# coding:utf-8
+# coding: utf-8
 import unittest
 from yandex_translate import YandexTranslate, YandexTranslateException
 
